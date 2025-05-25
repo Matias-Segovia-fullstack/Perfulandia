eb60d50ccc89af187a220381c6a0fd832194f331
Luciano Machuca <luc.machuca@duocuc.cl>
2025-05-24 21:06:38 -0400

    producto1

---
+++ msvc-producto/src/main/java/com/Perfulandia/msvc/producto/models/entities/Producto.java
@@ -0,0 +1,9 @@
+package com.Perfulandia.msvc.producto.models.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="productos")
+public class Producto {
+}
