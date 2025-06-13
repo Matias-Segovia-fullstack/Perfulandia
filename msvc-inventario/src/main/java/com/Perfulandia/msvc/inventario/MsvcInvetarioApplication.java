44116bb5c0cff7a73d5e6f42c6ff1a92f19311c0
Matias <mati.segovia@duocuc.cl>
2025-06-13 09:20:34 -0400

    Arreglo inventario

--- msvc-inventario/src/main/java/com/Perfulandia/msvc/inventario/MsvcInvetarioApplication.java
+++ msvc-inventario/src/main/java/com/Perfulandia/msvc/inventario/MsvcInvetarioApplication.java
@@ -2,8 +2,10 @@ package com.Perfulandia.msvc.inventario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.Perfulandia.msvc.inventario.clients")
 public class MsvcInvetarioApplication {
 
 	public static void main(String[] args) {
