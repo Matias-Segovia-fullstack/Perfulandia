0ce10f11778dbe08201fcee0f3f56113e8772976
Matias <mati.segovia@duocuc.cl>
2025-06-16 01:11:53 -0400

    application properties inventario

--- msvc-inventario/src/main/java/com/Perfulandia/msvc/inventario/clients/ProductoClient.java
+++ msvc-inventario/src/main/java/com/Perfulandia/msvc/inventario/clients/ProductoClient.java
@@ -6,9 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "msvc-producto", url = "${msvc.producto.url}")
-
 public interface ProductoClient {
-
     @GetMapping("/api/v1/productos/{id}")
     ProductoDTO obtenerProducto(@PathVariable("id") Long id);
 }
